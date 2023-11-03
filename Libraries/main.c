3c102619518e3e459ded9f0d3e948807d97cc089
Ilya Lopatin <mrmochin@yandex.ru>
2023-11-03 13:35:19 +0700

    Initial Commit

---
+++ Libraries/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Libraries
+//
+//  Created by Ilya Lopatin on 03.11.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
